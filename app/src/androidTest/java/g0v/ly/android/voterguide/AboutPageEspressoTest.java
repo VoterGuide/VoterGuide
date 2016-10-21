8ede31906b3a25cad839a1ddf6391d003a782ec9
HMW <wuheimin@gmail.com>
2016-10-21 18:56:23 +0800

    #46 Optimize imports

--- app/src/androidTest/java/g0v/ly/android/voterguide/AboutPageEspressoTest.java
+++ app/src/androidTest/java/g0v/ly/android/voterguide/AboutPageEspressoTest.java
@@ -6,14 +6,6 @@ import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.Espresso.pressBack;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -21,6 +13,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import g0v.ly.android.voterguide.ui.MainActivity;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 public class AboutPageEspressoTest {
